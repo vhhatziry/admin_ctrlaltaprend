1427a2607a95f8245fed8d671a72e02a985e8be4
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2025-01-01 18:26:22 -0600

    subtitulos

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const prevButton = document.getElementById('prev-section');
 const nextButton = document.getElementById('next-section');
 const contentSection = document.querySelector('.admin-content');
 const usersSection = document.querySelector('.admin-users');
+const title = document.querySelector('#admin-content-title');
 
 // Añadir evento click al botón "Siguiente"
 nextButton.addEventListener('click', () => {
     // Ocultar la sección actual con una animación (puedes personalizarla)
     contentSection.style.animation = 'slideOutRight 0.5s forwards'; 
+    title.textContent = "GESTIÓN DE USUARIOS";
 
     // Mostrar la siguiente sección después de un pequeño retraso
     setTimeout(() => {
@@ -20,6 +22,8 @@ nextButton.addEventListener('click', () => {
 // Añadir evento click al botón "Anterior" (similar al anterior)
 prevButton.addEventListener('click', () => {
     usersSection.style.animation = 'slideOutLeft 0.5s forwards';
+    title.textContent = "GESTIÓN DE CONTENIDO";
+
     setTimeout(() => {
         usersSection.style.display = 'none';
         contentSection.style.display = 'block';
